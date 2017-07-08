d8f1f74b0670fdb79208fe8fd8de57bb0a38300a
Thomas <thomas@geenbs.nl>
2017-07-08 21:09:28 +0200

    Function now returns ascii char for bin string

--- retAscciFrom6bits.c
+++ retAscciFrom6bits.c
@@ -29,36 +29,51 @@ unsigned int returnUIntFromBin(char *bin){
     return decimal;
 }
 
-
-
-
-
-
-
-
-
 typedef struct sixbitAsciiTable{
     char bits[6];
     char ascii;
 }sbat;
 
-//sbat sixbitAscii = malloc(65 * sizeof(sbat));
 sbat sixbitAscii[65] = {
-    {"000000", '@'},
-    {"000001", 'A'},
-    {"000010", 'B'},
+    {"000000", '@'},{"000001", 'A'},{"000010", 'B'},
+    {"000011", 'C'},{"000100", 'D'},{"000101", 'E'},
+    {"000110", 'F'},{"000111", 'G'},{"001000", 'H'},
+    {"001000", 'H'},{"001001", 'I'},{"001010", 'J'},
+    {"001011", 'K'},{"001100", 'L'},{"001101", 'M'},
+    {"001110", 'N'},{"001111", 'O'},{"100000", 'P'},
     };
 
 //return ascii value of six bit nibbles
 void returnAsciiFrom6bits(char *binString){
     size_t sz = strlen(binString);
-    printf("lngth of str: %d\n", strlen(binString));
 
-    printf("6bit ascii char value: %c\n", sixbitAscii[returnUIntFromBin(binString)].ascii);
+    if(sz % 6 != 0){
+        printf("Binary string is not a multiple of 6, so probably invalid\n");
+    }
+
+    size_t i = 0;
+    size_t j = 0;
+    size_t k = 0;
+    char nibble[6];
+    char *vesselname = (char *) malloc(sizeof(char) * (sz / 6) + 1);
+    for(i = 0; i < sz; i++){
+        nibble[j] = binString[i];
+        if(j == 5){
+            printf("6bit ascii char value: %c\n", sixbitAscii[returnUIntFromBin(nibble)].ascii);
+            vesselname[k] = sixbitAscii[returnUIntFromBin(nibble)].ascii;
+            j = 0;
+            k++;
+        }else{
+            j++;
+        }
+    }
+    printf("Vesselname: %s\n", vesselname);
+    free(vesselname);
 
 }
 
 int main(void){
-    char mystr[] = "000000"; //P
+    char mystr[] = "000111000101"; //P
     returnAsciiFrom6bits(mystr);
+    return 0;
 }
