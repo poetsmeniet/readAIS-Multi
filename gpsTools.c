d0b5258c0a820a2365487ad8ac71e659c59b9f11
Thomas <thomas@geenbs.nl>
2017-07-27 13:53:49 +0200

    Adding gpstools to project and adapted some code to calculate distance between coords

---
+++ gpsTools.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+#include "gpsTools.h"
+//Dont forget to link with -lm
+
+//code adapted from Salvador Dali, stackoverflow. Many thanks!
+//Applies haversine method between decimal degrees lat/lon
+//usage:
+//printf("%f\n", calcDistance(42.738705, -9.038047, 42.762424, -8.948540));
+double calcDistance(double lat1, double lon1, double lat2, double lon2) {
+    unsigned int R = 3440; // Radius of the earth in nm
+    double pi = 3.141592653589793; 
+    double dLat = (lat2 - lat1) * pi / 180;  
+    double dLon = (lon2 - lon1) * pi / 180;
+    double a = \
+       0.5 - cos(dLat)/2 + \
+       cos(lat1 * pi / 180) * cos(lat2 * pi / 180) * \
+       (1 - cos(dLon))/2;
+    
+    return R * 2 * asin(sqrt(a));
+}
---
+++ gpsTools.h
@@ -0,0 +1,7 @@
+#ifndef GPSTOOLS_H_
+#define GPSTOOLS_H_
+
+double calcDistance(double lat1, double lon1, double lat2, double lon2);
+
+#endif
+
